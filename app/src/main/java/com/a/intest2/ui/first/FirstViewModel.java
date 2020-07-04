cc787068fe5646e2122745de3f0f66ae2ca30e88
dm2911 <m291119@ua.fm>
2020-07-04 03:34:52 +0300

    Initial commit

---
+++ app/src/main/java/com/a/intest2/ui/first/FirstViewModel.java
@@ -0,0 +1,7 @@
+package com.a.intest2.ui.first;
+
+import androidx.lifecycle.ViewModel;
+
+public class FirstViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/a/intest2/ui/second/SecondViewModel.java
@@ -0,0 +1,7 @@
+package com.a.intest2.ui.second;
+
+import androidx.lifecycle.ViewModel;
+
+public class SecondViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
